66cbae5887b40b499dc89889229e6e8f92b2a48e
Nikolas <nikolasagl@hotmail.com>
2017-11-30 20:26:36 +0000

    seila

--- src/main/java/app/controllers/PessoasController.java
+++ src/main/java/app/controllers/PessoasController.java
@@ -1,5 +1,7 @@
 package app.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+
 import java.time.LocalDateTime;
 import org.javalite.activeweb.AppController;
 import org.javalite.activeweb.annotations.POST;
@@ -7,10 +9,10 @@ import org.javalite.activeweb.annotations.DELETE;
 import app.models.Pessoa;
 
 public class PessoasController extends AppController{
-	public void index(){
+	public void index(HttpServletRequest request) throws ServletException, IOException {
 		String smartcard = request.getParameter("smartcart");
 		String password = request.getParameter("password");
 		
-		out.print(smartcard + " " + password);
+		System.out.print(smartcard + " " + password);
 	}
 }
